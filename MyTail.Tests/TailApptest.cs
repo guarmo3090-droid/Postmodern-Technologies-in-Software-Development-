eb66bf233a256ccfe9d5d9ecbbe49609020b0e27
guarmo3090-droid <guarmo3090@gmail.com>
2026-03-28 00:05:00 +0000

    updated

--- MyTail.Tests/TailApptest.cs
+++ MyTail.Tests/TailApptest.cs
@@ -10,15 +10,12 @@ namespace MyTail.Tests
         [Test]
         public void Run_ValidStdinWithN_Returns0AndCorrectLines()
         {
-            // Arrange
             var input = new StringReader("line1\nline2\nline3\nline4\nline5\n");
             var output = new StringWriter();
             var error = new StringWriter();
 
-            // Act
             int exitCode = TailApp.Run(new[] { "-n", "2" }, input, output, error);
 
-            // Assert
             Assert.That(exitCode, Is.EqualTo(0));
             Assert.That(output.ToString().Replace("\r", ""), Is.EqualTo("line4\nline5\n"));
             Assert.That(error.ToString(), Is.Empty);
@@ -27,15 +24,12 @@ namespace MyTail.Tests
         [Test]
         public void Run_InvalidOptionFormat_Returns2AndError()
         {
-            // Arrange
             var input = new StringReader("test\n");
             var output = new StringWriter();
             var error = new StringWriter();
 
-            // Act
             int exitCode = TailApp.Run(new[] { "-n", "invalid" }, input, output, error);
 
-            // Assert
             Assert.That(exitCode, Is.EqualTo(2));
             Assert.That(output.ToString(), Is.Empty);
             Assert.That(error.ToString(), Does.Contain("valid positive numeric argument").IgnoreCase);
@@ -44,15 +38,12 @@ namespace MyTail.Tests
         [Test]
         public void Run_FileNotFound_Returns1AndError()
         {
-            // Arrange
             var input = new StringReader("");
             var output = new StringWriter();
             var error = new StringWriter();
 
-            // Act
             int exitCode = TailApp.Run(new[] { "nonexistent_file.txt" }, input, output, error);
 
-            // Assert
             Assert.That(exitCode, Is.EqualTo(1));
             Assert.That(output.ToString(), Is.Empty);
             Assert.That(error.ToString(), Does.Contain("not found").IgnoreCase);
